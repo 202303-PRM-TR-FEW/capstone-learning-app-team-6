39996ebd87445c4c8e4ba05b84a5399d250b4f38
wael kamira <ramondm4@gmail.com>
2023-07-04 21:49:38 +0300

    Categories Wael

--- app/page.js
+++ app/page.js
@@ -1,9 +1,37 @@
 import Image from "next/image";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+let category = [
+  { icon: "fa-solid fa-chart-line", name: "Sales" },
+  { icon: "fa-solid fa-users-line", name: "HR" },
+  { icon: "fa-solid fa-bezier-curve", name: "Drawing" },
+  { icon: "fa-solid fa-table", name: "Big Data" },
+  { icon: "fa-solid fa-pen-ruler", name: "Design" },
+  { icon: "fa-solid fa-lightbulb", name: "Markiting" },
+  { icon: "fa-solid fa-star", name: "Astronomy" },
+];
 
 export default function Home() {
   return (
     <main>
-      <h1>Project</h1>
+      <Categories />
     </main>
+  );
+}
+
+function Categories() {
+  return (
+    <div className="parent">
+      <ul className="container">
+        {category.map((category) => (
+          <>
+            <li className={category.icon}>
+              <p className="text">{category.name}</p>
+            </li>
+          </>
+        ))}
+      </ul>
+    </div>
   );
 }
