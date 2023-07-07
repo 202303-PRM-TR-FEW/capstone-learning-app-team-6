6758654ebe7c09b0163c54deb62eabdbd6d937f0
Mohamed Ibrahim <mohdayman22rr@gmail.com>
2023-07-07 22:25:06 +0300

    fix landing page (temporiraly)

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,21 @@
-import FeaturedCourses from "./Components/Home Page/featured courses/FeaturedCourses";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      {/* <h1>Project</h1>
-      <h1>Hello</h1>
-
-      <h1>Project</h1>
-      <h1>Hello</h1> */}
-
-      <FeaturedCourses></FeaturedCourses>
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1 style={{ fontSize: "24px", fontWeight: 600 }}>
+        This is the (landing) page
+        <br />
+        Try Navigating to{" "}
+        <a href="/home" style={{ color: "blue" }}>
+          /home
+        </a>
+      </h1>
     </main>
   );
 }
