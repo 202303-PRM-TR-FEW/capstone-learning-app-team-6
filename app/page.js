8bf2d7a2b4100d80ca422188d820152a4f12d1c2
Edriss572 <edriss_miakhel@yahoo.com>
2023-07-07 16:19:12 +0300

    The Recommendation in search part is complete

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Recommended from "@/components/Recommended";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-row">
       <Navbar/>
+      <Recommended/>
     </main>
   );
 }
