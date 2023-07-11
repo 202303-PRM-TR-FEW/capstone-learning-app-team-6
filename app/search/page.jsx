fe3e5f113ffeb8215c14b4c7aba9d8fcc636d2b4
wael kamira <ramondm4@gmail.com>
2023-07-11 19:11:28 +0300

    categories in search page done Wael

---
+++ app/search/page.jsx
@@ -0,0 +1,10 @@
+import CategoriesSearchPage from "app/Components/Search Page/categoriesSearchPage/categoriesSearchPage.jsx";
+const Search = (props) => {
+  return (
+    <main>
+      <CategoriesSearchPage />
+    </main>
+  );
+};
+
+export default Search;
