079c2fb342ed976f1096675670b6021e1733e253
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-07-31 18:14:24 +0200

    fix: fixed the design of recommended for you in the search page

--- Components/SearchPage/RecommendedForYou/Recommended.jsx
+++ Components/SearchPage/RecommendedForYou/Recommended.jsx
@@ -33,22 +33,25 @@ const courses = [
 
 const Recommended = () => {
   return (
-    <div className="max-w-full h-1/2 w-screen p-5">
-      <h3 className="text-slate-800 font-bold">RECOMMENDED FOR YOU</h3>
-      <div className="flex flex-row flex-wrap gap-4 ml-2">
+    <div className="max-w-full w-5/6 h-1/2 pt-10 p-x-5">
+      <h3 className="text-slate-800 text-xl font-bold">RECOMMENDED FOR YOU</h3>
+      <div className="flex flex-row flex-wrap gap-6 p-3 ml-2">
         {/* the loop start */}
         {courses.map((course) => (
-          <div key={course.id}>
-            <Image
-              className="mt-5  rounded-xl"
+          <div
+            className="cursor-pointer hover:outline hover:outline-2 hover:outline-blue-600 mt-5 rounded-2xl"
+            key={course.id}
+          >
+            <img
+              className="object-fill w-72 h-60 rounded-xl"
               src={course.image}
-              width={200}
-              height={2}
               alt="Picture of the author"
             />
 
-            <h2 className="text-black font-bold text-l mt-3">{course.name}</h2>
-            <p className="text-gray-500 text-xs">{course.author}</p>
+            <h2 className="text-black font-bold text-xl pl-2 mt-3">
+              {course.name}
+            </h2>
+            <p className="text-gray-500 text-sm pl-2">{course.author}</p>
           </div>
         ))}
         {/* the loop ends */}
