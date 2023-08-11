e21fcd1530418ca65da50c9c8f68f0721e087aec
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-08-11 13:07:02 +0200

    fix: resolving issues with auth page

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,14 @@
-"use client";
-
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation"; 
-/* import { auth, googleProvider } from "../../firebase";
-import { signInWithPopup } from "firebase/auth";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
- */
+
 const courses = [
   {
     id: "1",
     name: "Building ios15 App",
     author: "Tom Collins",
-    category: "Programming",
-    description: "Learn to build iOS apps using the latest technologies.",
+    category: "test",
+    description: "test",
     duration: "1h 17min",
     level: "Beginner",
     price: 24,
@@ -41,8 +30,8 @@ const courses = [
     author: "Clara Manning",
     value: 69,
     category: "Photography",
-    description: "Master the art of photography and capture stunning moments.",
-    rate: 2.8,
+    description: "test",
+    rate: 5.0,
     duration: "2h",
     price: 24,
     level: "Intermediate",
@@ -61,9 +50,9 @@ const courses = [
     name: "Design Thinking 2.0",
     author: "Chris Kinley",
     value: 27,
-    category: "Design",
-    description: "Learn advanced design thinking principles and methodologies.",
-    rate: 3.6,
+    category: "Content Making",
+    description: "test",
+    rate: 5.0,
     duration: "2h",
     price: 24,
     level: "Professional",
@@ -83,7 +72,7 @@ const courses = [
     author: "Lena Gold",
     value: 11,
     category: "Photography",
-    description: "Learn techniques for capturing stunning product photographs.",
+    description: "test",
     rate: 5.0,
     duration: "2h",
     price: 24,
@@ -104,8 +93,8 @@ const courses = [
     author: "Maria Silver",
     value: 27,
     category: "Photography",
-    description: "Discover the art of capturing captivating portraits.",
-    rate: 2,
+    description: "test",
+    rate: 5.0,
     duration: "2h",
     price: 24,
     level: "Professional",
@@ -125,8 +114,8 @@ const courses = [
     author: "Gary Saltz",
     value: 27,
     category: "Photography",
-    description: "Create your own typefaces with this comprehensive course.",
-    rate: 4.6,
+    description: "test",
+    rate: 5.0,
     duration: "2h",
     price: 24,
     level: "Intermediate",
@@ -145,9 +134,9 @@ const courses = [
     name: "Excel: Formulas and functions",
     author: "Mike Curtis",
     value: 27,
-    category: "Marketing",
-    description: "Master Excel formulas and functions for data analysis.",
-    rate: 1.3,
+    category: "Excel",
+    description: "test",
+    rate: 5.0,
     duration: "2h",
     price: 24,
     level: "Professional",
@@ -165,12 +154,12 @@ const courses = [
     id: "8",
     name: "UX Essentials",
     author: "Don Draper",
-    category: "Design",
-    description: "Learn essential concepts of User Experience (UX) design.",
+    category: "UX",
+    description: "Test",
     duration: "1h 44min",
     level: "Professional",
     price: 16.99,
-    rate: 3.5,
+    rate: 4.2,
     value: 10,
     image:
       "https://images.unsplash.com/photo-1569098644956-02996eb711b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw4MHx8dXh8ZW58MXx8fHwxNjczMDE4Nzcy&ixlib=rb-4.0.3&q=80&w=1080",
@@ -186,12 +175,12 @@ const courses = [
     id: "9",
     name: "UX Research",
     author: "Chris Kinley",
-    category: "Design",
-    description: "Learn essential concepts of User Experience (UX) design.",
+    category: "UX",
+    description: "Test",
     duration: "1h 44min",
     level: "Intermediate",
     price: 16.99,
-    rate: 0.8,
+    rate: 4.2,
     value: 10,
     image:
       "https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwyfHx1eCUyMGRlc2lnbiUyMHxlbnwxfHx8fDE2NzMwMTc0MDg&ixlib=rb-4.0.3&q=80&w=1080",
@@ -207,12 +196,12 @@ const courses = [
     id: "10",
     name: "UX for Beginners",
     author: "Ben Starter",
-    category: "Drawing",
-    description: "Learn essential concepts of User Experience (UX) design.",
+    category: "UX",
+    description: "Test",
     duration: "1h 44min",
     level: "Beginner",
     price: 16.99,
-    rate: 2,
+    rate: 4.2,
     value: 10,
     image:
       "https://images.unsplash.com/photo-1618788372246-79faff0c3742?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw0fHx1eCUyMGRlc2lnbnxlbnwxfHx8fDE2NzMwMTkwOTQ&ixlib=rb-4.0.3&q=80&w=1080",
@@ -228,12 +217,12 @@ const courses = [
     id: "11",
     name: "How to UX?",
     author: "Anna May",
-    category: "HR",
-    description: "Learn the fundamentals of User Experience (UX) design.",
+    category: "UX",
+    description: "Test",
     duration: "1h 44min",
     level: "Beginner",
     price: 16.99,
-    rate: 3.2,
+    rate: 4.2,
     value: 10,
     image:
       "https://images.unsplash.com/photo-1547027072-332f09bd6bb3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw0Mnx8dXglMjBkZXNpZ258ZW58MXx8fHwxNjczMDE5MTc5&ixlib=rb-4.0.3&q=80&w=1080",
@@ -244,127 +233,47 @@ const courses = [
       { name: "Test", videos: 2 },
       { name: "Skill Test", videos: 1 },
     ],
-  }
+  },
 ];
 
-const FirstPage = () => {
-
-//  const router = useRouter();
-//  const auth = getAuth();
-  
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  
-  const handleSignup = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        router.push("/home");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        router.push("/home");
-      })
-      .catch((error) => {
-        setError("Incorrect email or password!");
-        console.error(error);
-      });
-  };
-
-  const handleGoogleLogin = () => {
-    signInWithPopup(auth, googleProvider)
-      .then(() => {
-        router.push("/home");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const navigateToHomePage = () => {
-    router.push("/");
-  };
-
+const firstPage = () => {
   return (
     <div className="flex bg-gray-300 w-screen h-screen">
-      <div className="w-1/2 flex items-center justify-center h-screen p-2">
-        <div className="flex flex-col bg-white mx-auto w-3/6 h-54 rounded-xl">
-          <div
-            className="text-blue-500 mx-auto m-10 mb-5 hover:text-blue-400 cursor-pointer"
-            onClick={navigateToHomePage}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-14 h-14"
-            >
-            <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
-            <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
-            <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
-          </svg>
-        </div>
-
-        
-        <h1 className="mb-4 mx-auto text-4xl font-semibold">Welcome to LearnU</h1>
-          {error && <div className={styles.error}> {error} </div>}{" "}
-          {/* error message */}
-
-          <input
-            className="h-20 outline outline-2 outline-slate-400 focus:outline-blue-400 text-2xl rounded p-1 pl-4 m-10"
-            type="text" 
-            placeholder="Email or Phone number"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <input 
-            className="h-20 outline outline-2 outline-slate-400 focus:outline-blue-400 text-2xl rounded p-1 pl-4 m-10 mt-0"
-            type={passwordVisible ? "text" : "password"}
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}            
-          />
-
-          <input
-            className="bg-blue-600 hover:bg-blue-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10 mt-0"
-            type="submit"
-            onClick={handleLogin}
-            value="Log In"
-          />
-
-          <p
-            className="m-10 mb-5 mx-auto mt-0 hover:underline cursor-pointer text-blue-500 font-semibold text-xl "
-          >
-            Forgot Password ?
-          </p>
-          <hr
-            className="bg-slate-400 mx-auto mt-0 w-1/2 h-1 rounded"
-          />
-
-          <input
-            className="bg-green-600 hover:bg-green-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10"
-            type="button"
-            onClick={handleSignup}
-            name="" 
-            id="" 
-            value="Create New Account"
-          />
-
-          <input
-            className="bg-red-600 hover:bg-red-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10 mt-0"
-            type="button"
-            onClick={handleGoogleLogin}
-            value="Log In with Google"
-          />
-
+      <div className="w-1/2 flex items-center justify-center h-screen">
+        <div className="flex flex-col mx-auto w-3/6 h-54">
+          <div className="flex text-blue-500">
+            <div className="text-blue-500 hover:text-blue-400 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-10 h-10 mx-auto"
+              >
+                <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+                <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+                <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+              </svg>
+            </div>
+            <p className="font-semibold content-center text-lg text-center m-2">
+              LearnU
+            </p>
+          </div>
+          <div className="mt-14">
+            <h2 className="text-5xl font-semibold tracking-wide">
+              Discover Passion
+            </h2>
+            <p className="text-xl font-semibold text- tracking-norml mt-8">
+              Find out what topics you find interesting, learn a new skills &
+              connect with people that are Passion about similar topic
+            </p>
+          </div>
+          <div>
+            <Link href="/signin">
+              <button className="bg-blue-600 hover:bg-blue-500 text-white text-lg p-1 font-bold w-52 h-12 mt-10 tracking-wide rounded-xl">
+                GET STARTED
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -381,4 +290,5 @@ const FirstPage = () => {
   );
 };
 
-export default FirstPage;
+export default firstPage;
+export { courses };
--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { auth, googleProvider } from "../../firebase";
 import { signInWithPopup } from "firebase/auth";
 import {
@@ -58,70 +60,104 @@ const LoginPage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles["login-box"]}>
-        <h1 className="mb-4 text-xl font-semibold">Welcome to LearnU</h1>
-        <div
-          className="text-blue-500 hover:text-blue-400 cursor-pointer"
-          onClick={navigateToHomePage}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-10 h-10"
+    <div className="flex bg-gray-300 w-screen h-screen">
+      <div className="w-1/2 flex items-center justify-center h-screen p-2">
+        <div className="flex flex-col bg-white mx-auto w-3/6 h-54 rounded-xl">
+          <div
+            className="text-blue-500 mx-auto m-10 mb-5 hover:text-blue-400 cursor-pointer"
+            onClick={navigateToHomePage}
           >
-            <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
-            <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
-            <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
-          </svg>
-        </div>
-        {error && <div className={styles.error}> {error} </div>}{" "}
-        {/* error message */}
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className={styles["input-field"]}
-        />
-        <div className={styles["password-container"]}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-14 h-14"
+            >
+              <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+              <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+              <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+            </svg>
+          </div>
+          <h1 className="mb-4 mx-auto text-4xl font-semibold">
+            Welcome to LearnU
+          </h1>
+          <div className="flex flex-col items-center justify-center">
+            {error && (
+              <div className="text-xl text-red-600 mt-2 text-center">
+                {" "}
+                {error}{" "}
+              </div>
+            )}
+          </div>
           <input
+            className="h-20 outline outline-2 outline-slate-400 focus:outline-blue-400 text-2xl rounded p-1 pl-4 m-10"
+            type="text"
+            placeholder="Email or Phone number"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              if (!e.target.value && !password) {
+                setError(null);
+              }
+            }}
+          />
+          <input
+            className="h-20 outline outline-2 outline-slate-400 focus:outline-blue-400 text-2xl rounded p-1 pl-4 pr-10 m-10 mt-0"
             type={passwordVisible ? "text" : "password"}
             placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={styles["input-field"]}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              if (!e.target.value && !email) {
+                setError(null);
+              }
+            }}
           />
           <span
             onClick={() => setPasswordVisible(!passwordVisible)}
-            className={styles["toggle-password-visibility"]}
+            className={`${styles["toggle-password-visibility"]} cursor-pointer `}
           >
             {passwordVisible ? (
-              <AiOutlineEye size={18} />
-            ) : (
               <AiOutlineEyeInvisible size={18} />
+            ) : (
+              <AiOutlineEye size={18} />
             )}
           </span>
+          <input
+            className="bg-blue-600 hover:bg-blue-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10 mt-0"
+            type="submit"
+            onClick={handleLogin}
+            value="Log In"
+          />
+          {/* <p className="m-10 mb-5 mx-auto mt-0 hover:underline cursor-pointer text-blue-500 font-semibold text-xl ">
+            Forgot Password ?
+          </p> */}
+          <hr className="bg-slate-400 mx-auto mt-0 w-1/2 h-1 rounded" />
+          <input
+            className="bg-green-600 hover:bg-green-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10"
+            type="button"
+            onClick={handleSignup}
+            name=""
+            id=""
+            value="Sign Up"
+          />
+          <input
+            className="bg-red-600 hover:bg-red-500 text-white font-semibold text-2xl h-20 rounded-xl pl-4 m-10 mt-0"
+            type="button"
+            onClick={handleGoogleLogin}
+            value="Log In with Google"
+          />
         </div>
-        <button
-          onClick={handleSignup}
-          className={`${styles.button} ${styles["button-signup"]}`}
-        >
-          Sign Up
-        </button>
-        <button
-          onClick={handleLogin}
-          className={`${styles.button} ${styles["button-login"]}`}
-        >
-          Log In
-        </button>
-        <button
-          onClick={handleGoogleLogin}
-          className={`${styles.button} ${styles["button-google"]}`}
-        >
-          Log In with Google
-        </button>
+      </div>
+
+      <div className="flex bg-white rounded-l-[70px] items-center justify-center h-screen w-1/2">
+        <Image
+          className=""
+          src="https://assets.api.uizard.io/api/cdn/stream/a8ce660d-47da-404a-b3b3-63ca6970ddcf.png"
+          width={500}
+          height={500}
+          alt="front page"
+        />
       </div>
     </div>
   );
